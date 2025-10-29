4fd907f6974740c90ec806c761423d248a3acb01
norinori <noriyuki_okochi@ybb.ne.jp>
2025-10-29 21:04:25 +0900

    ヘルプメッセージ修正

--- src/chart.py
+++ src/chart.py
@@ -45,7 +45,8 @@ key_names:str = [name for name in Kn2idx]
 opts:str = [opt for opt in args if opt.startswith('-')]
 if '-h' in opts:        #debug write
     print("chart.py -case {-L(ist)|'<case-name1>[,<case_name2>']} [-D(elete)] [-import [<csv-file-path>]] \n"\
-         + "        {<key_name1>|[ <key_name2>...]|*} [-range '<min>[,<max>']] [-second <col_name>] [-span] [-SMA <window>] [-WMA <window>]\n"\
+         + "        {<key_name1>|[ <key_name2>...]|*} [-range '<min>[,<max>']] \n"\
+         + "        [-second <col_name>] [-span] [-SMA <window>] [-WMA <window>]\n"\
          + "        [{-p(ast-frames)|-f(irst-frame)}'<count1>[,<count2>']] [<display-frames-count>] \n"\
          + "        [-m(ulti)] [-b(ottom)] [-s(lider)] [-h(elp)] [-d(ebug)]")
     exit(0)
