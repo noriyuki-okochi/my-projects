7b104abecc919e341fc87684ea7d53804c64479e
norinori <noriyuki_okochi@ybb.ne.jp>
2026-08-22 08:59:21 +0900

    ・アラートID、メッセージ定義ファイルを移動（env.py -> param.py）

--- src/kyudo/param.py
+++ src/kyudo/param.py
@@ -277,6 +277,31 @@ Eyes_ratio_max:float = 0.1              # 目幅比率の最大値
 Eyes_ratio_min:float = 0.01             # 目幅比率の最小値
 #
 #
+# アラートID、メッセージの定義
+#
+Alart_Asibumi= 10     # 「正対不完全」のアラートID
+Alart_Monomi = 30     # 「物見を定まらず」のアラートID
+Alart_Daisan = 40     # 「大三移行不安定」のアラートID
+Alart_KaiNasi = 50    # 「会なし離れ」のアラートID
+Alart_KaiFusoku = 60  # 「会不十分な離れ」のアラートID
+Alart_Hanare = 70     # 「離れタイミングずれ」のアラートID
+#
+Alart_msg = {
+   0:'',
+   10:'Warning:Detected illegal action in section-1.(SEITAI fukanzen)',
+   100:'<警告>：「正対不完全」を検知しました。',
+   30:'Warning:Detected illegal action in section-3.(MONOMI sadamarazu)',
+   300:'<警告>：「物見定まらず」を検知しました。',
+   40:'Warning:Detected illegal action in section-5.(DAISAN fumeikaku)',
+   400:'<警告>：「大三移行不安定」を検知しました。',
+   50:'Warning:Detected illegal action in section-5.(KAI nasi)',
+   500:'<警告>：「会なし離れ」を検知しました。',
+   60:'Warning:Detected illegal action in section-6.(KAI fusoku)',
+   600:'<警告>：「会不十分な離れ」を検知しました。',
+   70:'Warning:Detected illegal action in section-7.(Timing un-match)',
+   700:'<警告>：「弓手押しタイミングの遅れ」を検知しました。'
+}
+#
 # 評価の減点条件のパラメータ定義
 #
 Diduct_params = {
